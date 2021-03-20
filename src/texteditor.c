70a3797a1246ff391b02364005300b00f3da2243
inFamousxD <audimankar@gmail.com>
2021-03-20 12:54:59 +0530

    C4/S72 : Column scrolling

--- src/texteditor.c
+++ src/texteditor.c
@@ -38,6 +38,7 @@ typedef struct erow {
 struct editorConfig {
     int cx, cy;
     int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -347,6 +348,7 @@ void initEditor() {
     E.cx = 0;
     E.cy = 0;
     E.numrows = 0;
+    E.coloff = 0;
     E.row = NULL;
     E.rowoff = 0;
 
